b8a39011c321f3363ead173fd4d0da446ec90d2b
mubashir odho <122596257+ODHO@users.noreply.github.com>
2024-03-11 18:50:06 +0500

    Update index.php
    ssss

--- index.php
+++ index.php
@@ -52,75 +52,8 @@ if(isset($_POST['submitCV'])) {
 <title>About US</title>
 <body>
 
-    <div class="aboutusbanner">
-      <div class="container">
-        <div class="services-section-heading">
-          <h1>about <span>us</span></h1>
-        </div>
-        <img src="./assets/images/Group 20.png" alt="" class="lines-dot-left">
-        <div class="text-center">
-          Who We Are
-        </div>
-        <img src="./assets/images/Group 20.png" alt="" class="lines-dot-right">
-        <div class="bannerimage">
-          <img src="./assets/images/aboutusbanner.png" alt="image">
-        </div>
-        <div class="parasection">
-          <p>At the heart of our identity lies a relentless commitment to surpassing the ordinary. We are not content with the bare minimum; instead, we relentlessly pursue perfection. Your satisfaction is not just a goal for us; it's our guiding principle. Every endeavor is a testament to our unwavering dedication to meeting and exceeding your expectations. We immerse ourselves in the digital realm, embodying the spirit of a born-digital company. Understanding the unique and evolving needs of our clients is ingrained in our DNA, and we tirelessly strive to fulfill those needs to the fullest extent.</p>
-          <p>Digital innovation courses through our veins, defining our essence as a company that was born in the digital era. We don't just work hard; we bleed digital, symbolizing our deep-rooted connection to the dynamic landscape of the online world. Our pride stems from the exceptional quality of our work, and witnessing your business thrive is the ultimate testament to our boundless creativity. We approach each project as an opportunity to elevate our craft, pushing the boundaries of what is possible to unlock new levels of ingenuity and originality.</p>
-          <p>Our dedication to excellence goes beyond mere rhetoric; it is a driving force that fuels our passion for what we do. We take immense pride in our craft, viewing each client's success as a reflection of our unwavering commitment to delivering unparalleled results. Your business flourishing isn't just a goal; it propels us to new heights of creativity, inspiring us to continually refine and redefine the standards of excellence in our industry. We are not just a service provider; we are your dedicated partner in achieving and surpassing your digital aspirations.</p>
-        </div>
-      </div>
-    </div>
-    <div class="aboutbox">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="boxes">
-              <img src="./assets/images/boxpic.png" alt="image">
-              <div class="boxestext">
-
-                <h3>What <span> We </span> Do?</h3>
-                <p>Our dynamic professional team of talented designers, developers, programmers and marketing professionals to drive your best business forward and take you to where you need to be.</p>
-              </div>
-              </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="boxes">
-              <img src="./assets/images/boxpic.png" alt="image">
-              <div class="boxestext">
-
-                <h3>Why <span> Choose </span> Us?</h3>
-                <p>
-                  Understanding the complete needs of our clients to the fullest interpreting them in a digital language and achieving a complete satisfaction is what we were created to do.
-                </p>
-              </div>
-              </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="boxes">
-              <img src="./assets/images/boxpic.png" alt="image">
-              <div class="boxestext">
-
-                <h3>Who <span>We </span> Help?</h3>
-                <p>
-                  We instigate sustainable and measurable processes to help seize today’s digital marketplace, to help grow your business ROI.
-                </p>
-              </div>
-              </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="scheduleSec">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-9">
-            <h1>What’s <span>next</span>?</h1>
-            <p>Collaboration begins with a conversation. Whether you’re interested in discussing a new project or simply wish to find out more about our services and what we can do for you, we are here to help. Let’s get the conversation going.</p>
-          </div>
-          
-          <div class="col-lg-3">
+   
+   <div class="col-lg-3">
           <a type="button" class="btn btn-primary button-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Start a project</a>
 
           <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal for @mdo</button> -->
@@ -159,11 +92,6 @@ if(isset($_POST['submitCV'])) {
   </div>
 </div>
           </div>
-        </div>
-      </div>
-    </div>
-    
-    
         
 
 </body>
